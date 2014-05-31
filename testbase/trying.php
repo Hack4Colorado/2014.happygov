dc0d07a1e267267879b1b48679d00272514e31ce
alex <anovoselsky@rentbits.com>
2014-05-31 15:41:09 -0600

    mysqli

--- testbase/trying.php
+++ testbase/trying.php
@@ -1,3 +1,16 @@
+<?php
+$con = mysqli_connect("192.254.236.167:3306","candm_happgov","happygov1!","candm_happygov");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  echo $con->host_info . "\n";
+?>
+
+
 <ul>
 <li>Coffee</li>
 <li>Milk</li>
