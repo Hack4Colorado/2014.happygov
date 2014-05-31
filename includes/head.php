1b74fc1ac99f013654222c79201ab60d8405c55d
Chris Holder <chris@see8ch.com>
2014-05-31 14:55:38 -0600

    Basic Structure

---
+++ includes/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HappyGov</title>
+	<meta charset="utf-8">
+	<meta name="description" content="" />
+	<meta name="author" content="">
+	<meta name="keywords" content="" />
+
+
+<!-- Optimized mobile viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+
+<!--	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/logo.40x40png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/logo.80x80.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/logo.128x128.png">	-->
+
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+	<script src="js/html5.js"></script>
+   <script>
+      document.createElement('header');
+      document.createElement('section');
+      document.createElement('footer');
+   </script>
+<![endif]-->
+
+</head>
+
+<body id="home">
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('includes/head.php'); ?>
+
+<?php include('includes/nav.php'); ?>
+
+<!-- Begin Content -->
+<div class="content-wrapper">
+
+</div>
+
+
+<?php include('includes/footer.php'); ?>
