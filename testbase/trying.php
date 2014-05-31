eef4c80d096f53caf0fcbe2baf144801a54b1a9c
alex <anovoselsky@rentbits.com>
2014-05-31 15:43:53 -0600

    change mysqli

--- testbase/trying.php
+++ testbase/trying.php
@@ -1,13 +1,10 @@
 <?php
-$con = mysqli_connect("192.254.236.167:3306","candm_happgov","happygov1!","candm_happygov");
+$mysqli = new mysqli("192.254.236.167", "candm_happgov", "happygov1!", "candm_happygov", 3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
 
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  
-  echo $con->host_info . "\n";
 ?>
 
 
